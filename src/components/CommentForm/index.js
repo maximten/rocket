2a185b66eced11be4601a92cb7357f3618c51adf
Maxim Ten <maximten94@gmail.com>
2018-02-08 00:19:03 +0700

    validation added

--- src/components/CommentForm/index.js
+++ src/components/CommentForm/index.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import 'formdata-polyfill';
+import _ from 'lodash';
+import classNames from 'classnames';
 import './index.less';
+import { error } from 'util';
 
 export default class CommentForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      errors: {
+        name: false,
+        content: false
+      }
+    };
+  }
   submit = (e) => {
     e.preventDefault();
     const { postId, add } = this.props;
     const formData = new FormData(e.target);
     const name = formData.get('name');
-    const content = formData.get('message');
-    add(postId, name, content);
+    const content = formData.get('content');
+    const errors = {
+      name: !name,
+      content: !content
+    };
+    const errorsCount = _.reduce(errors, (carry, item) => { 
+      return carry + item;
+    }, 0);
+    this.setState({ errors })
+    if (!errorsCount) {
+      add(postId, name, content);
+    }
   }
   render() {
+    const { errors } = this.state;
+    const nameFormClass = classNames("form-group", { "has-error": errors.name });
+    const contentFormClass = classNames("form-group", { "has-error": errors.content });
     return (
       <form className="comment-form" onSubmit={this.submit}>
         <div className="left">
@@ -20,11 +45,11 @@ export default class CommentForm extends Component {
           </button>
         </div>
         <div className="right">
-          <div className="form-group">
+          <div className={ nameFormClass }>
             <input name="name" type="text" placeholder="Name"/>
           </div>
-          <div className="form-group">
-            <input name="message" placeholder="Message"/>
+          <div className={ contentFormClass }>
+            <input name="content" placeholder="Message"/>
           </div>
         </div>
       </form>
